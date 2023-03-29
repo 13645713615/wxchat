800ee1c4e3989049f3a0a8d11650a23d90dca700
Carroll <1186684149@qq.com>
2023-03-30 02:52:34 +0800

    FIX 修改素材库

--- service/medium.ts
+++ service/medium.ts
@@ -3,16 +3,21 @@
  * @version: 
  * @Author: Carroll
  * @Date: 2023-03-23 02:13:41
- * @LastEditTime: 2023-03-29 18:54:31
+ * @LastEditTime: 2023-03-30 02:49:49
  */
 import { speech } from "baidu-aip-sdk-ts"
 import FormData from "form-data";
 import axios from "axios"
 import { BAIDU_API_KEY, BAIDU_APP_ID, BAIDU_SECRET_KEY } from "../config";
 import { getAccessToken } from "./accessToken";
+import redisClient from '../utils/redis';
+import day from "dayjs";
+import schedule from "node-schedule"
+import logger from "../utils/logger";
+
+const client = redisClient();
 
 const baiduClient = new speech(BAIDU_APP_ID, BAIDU_API_KEY, BAIDU_SECRET_KEY)
-
 
 // 上传语音到微信素材库
 export const uploadVoice = async (content: string): Promise<string> => {
@@ -27,7 +32,7 @@ export const uploadVoice = async (content: string): Promise<string> => {
     const form = new FormData()
     form.append("media", result.data, { filename: "voice.mp3", contentType: "audio/mpeg" });
 
-    const url = `https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${await getAccessToken()}&type=voice`;
+    const url = `https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=${await getAccessToken()}&type=voice`;
 
     // 上传语音
     const { data } = await axios.post(url, form, { headers: form.getHeaders() })
@@ -36,4 +41,47 @@ export const uploadVoice = async (content: string): Promise<string> => {
     }
 
     return data.media_id;
+}
+
+
+// 把素材保存在redis中，零时保存
+export const saveMaterial = async (mediaId: string) => {
+    const redis = client.getRedis();
+    const time = day().add(10, "minute")
+    const key = `material:${day(time.format("YYYY-MM-DD HH:mm")).unix()}`
+    await redis.sadd(key, mediaId);
+}
+
+
+// 清除一个小时之前的素材
+export const clearMaterial = async () => {
+    const redis = client.getRedis();
+    const time = day();
+    const key = `material:${day(time.format("YYYY-MM-DD HH:mm")).unix()}`
+    const mediaIds = await redis.smembers(key);
+    clearAll(mediaIds);
+}
+
+
+// 批量删除素材
+export const clearAll = (mediaIds: string[]) => {
+
+    if (mediaIds.length === 0) return;
+
+    Promise.all(mediaIds.map((mediaId) => deleteVoice(mediaId))).then(() => {
+        logger.info("素材清理成功:" + mediaIds.length)
+    }).catch((error) => {
+        logger.error(error.message)
+    })
+}
+
+// 删除微信素材库中的语音
+export const deleteVoice = async (mediaId: string) => {
+
+    const url = `https://api.weixin.qq.com/cgi-bin/material/del_material?access_token=${await getAccessToken()}`;
+
+    const { data } = await axios.post(url, { media_id: mediaId })
+    if (data.errcode) {
+        throw new Error(`语音删除失败: ${data.errmsg}`)
+    }
 }
