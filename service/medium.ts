871b48c3687c932b475f166e6e6f75cc051c52f5
Carroll <1186684149@qq.com>
2023-03-31 17:34:10 +0800

    FIX 回滚，微信临时素材

--- service/medium.ts
+++ service/medium.ts
@@ -3,13 +3,14 @@
  * @version: 
  * @Author: Carroll
  * @Date: 2023-03-23 02:13:41
- * @LastEditTime: 2023-03-29 18:01:48
+ * @LastEditTime: 2023-03-31 16:46:00
  */
 import { speech } from "baidu-aip-sdk-ts"
 import FormData from "form-data";
 import axios from "axios"
 import { BAIDU_API_KEY, BAIDU_APP_ID, BAIDU_SECRET_KEY } from "../config";
 import { getAccessToken } from "./accessToken";
+import logger from "../utils/logger";
 
 const baiduClient = new speech(BAIDU_APP_ID, BAIDU_API_KEY, BAIDU_SECRET_KEY)
 
@@ -19,7 +20,7 @@ export const uploadVoice = async (content: string): Promise<string> => {
 
     const url = `https://api.weixin.qq.com/cgi-bin/media/upload?access_token=${await getAccessToken()}&type=voice`;
     // 语音合成
-    const result = await baiduClient.text2audio(content, { spd: 5, per: 4 })
+    const result = await baiduClient.text2audio(content, { spd: 6, per: 4, vol: 10 })
     if (result.err_no) {
         throw new Error(`语音上传失败: ${result.err_msg}`)
     }
@@ -36,6 +37,8 @@ export const uploadVoice = async (content: string): Promise<string> => {
     if (data.errcode) {
         throw new Error(`语音上传失败: ${data.errmsg}`)
     }
-    
+
+    logger.info(`上传语音成功，MediaId: ${data.media_id}`);
+
     return data.media_id;
 }
